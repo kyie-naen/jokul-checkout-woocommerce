e9404fb6f62546ef893a751522bf931ed203006e
Rizky Zulkarnaen <90580486+kyie-naen@users.noreply.github.com>
2021-11-02 15:10:37 +0800

    change url

--- Module/JokulBased.php
+++ Module/JokulBased.php
@@ -1,16 +1,14 @@
 <?php
 
 class JokulBased {
-
-  const SANDBOX_BASE_URL    = 'https://api-sandbox.doku.com';
-  const PRODUCTION_BASE_URL = 'https://api.doku.com';
-
+  
   /**
    * @return string Doku API URL, depends on $state
    */
   public function getBaseUrl($state)
   {
-    return ($state == 'production') ? JokulConfig::PRODUCTION_BASE_URL : JokulConfig::SANDBOX_BASE_URL;
+    // return $state;
+    return ($state == 'production') ? 'https://api.doku.com' : 'https://api-sandbox.doku.com';
   }
 }
 
